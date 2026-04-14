62a5d6d70d42d101be23d9508e3b69b1e8c7bae9
Michell Klinger <michellklinger@gmail.com>
2026-04-13 19:44:08 -0500

    se crea el formulario
    se crea formulario para los datos de entrada de acuerdo con el tipo de metodo y se modifica el index

---
+++ tesis_math_app/web/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class SimuladorForm(forms.Form):
+    METODOS = [
+        ('CN', 'Crank-Nicolson'),
+        ('DF', 'Diferencias Finitas (Próximamente)'),
+    ]
+    
+    metodo = forms.ChoiceField(
+        choices=METODOS, 
+        label="Método de Cálculo",
+        widget=forms.Select(attrs={'class': 'form-select', 'onchange': 'this.form.submit()'})
+    )
+    
+    # Campos de entrada
+    k1 = forms.FloatField(label='Permeabilidad Capa 1 (k1)', initial=2e-11)
+    k2 = forms.FloatField(label='Permeabilidad Capa 2 (k2)', initial=10e-12)
+    t_max = forms.IntegerField(label='Tiempo Máximo (s)', initial=5000)
+    dt = forms.FloatField(label='Paso de tiempo (dt)', initial=1.0)
+    sw_inj = forms.FloatField(label='Saturación Inyección', initial=0.372)
+    sw_ini = forms.FloatField(label='Saturación Inicial', initial=0.72)
--- tesis_math_app/web/views.py
+++ tesis_math_app/web/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+from .forms import SimuladorForm
+from core.simulador import ejecutar_simulacion
 
 # Create your views here.
 def index(request):
-    return render(request, 'web/index.html')
+    # Inicializamos el formulario con los datos que vengan por POST o vacíos
+    form = SimuladorForm(request.POST or None)
+    resultados = None
+    mensaje = None
 
-    
+    if request.method == 'POST' and form.is_valid():
+        metodo_elegido = form.cleaned_data['metodo']
+        
+        if metodo_elegido == 'CN':
+            # Ejecutamos Crank-Nicolson (Punto 1)
+            params = {
+                'k1': form.cleaned_data['k1'],
+                'k2': form.cleaned_data['k2'],
+                'Tmax': form.cleaned_data['t_max'],
+                'dt': form.cleaned_data['dt'],
+                'Sw_inj': form.cleaned_data['sw_inj'],
+                'Sw_ini': form.cleaned_data['sw_ini'],
+            }
+            resultados = ejecutar_simulacion(params)
+        else:
+            mensaje = "El método de Diferencias Finitas está en desarrollo."
+
+    return render(request, 'web/index.html', {
+        'form': form, 
+        'resultados': resultados,
+        'mensaje': mensaje
+    })
