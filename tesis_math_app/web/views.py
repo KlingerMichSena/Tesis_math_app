22088ae7a5cc2f8fe5435c0095d4f72ea935a9cd
Michell Klinger <michellklinger@gmail.com>
2026-04-21 21:07:51 -0500

    cambio de la logica del formulario

--- tesis_math_app/web/views.py
+++ tesis_math_app/web/views.py
@@ -1,33 +1,43 @@
+import plotly.graph_objects as go
+from plotly.offline import plot
+import json
 from django.shortcuts import render
 from .forms import SimuladorForm
 from core.simulador import ejecutar_simulacion
 
-# Create your views here.
 def index(request):
-    # Inicializamos el formulario con los datos que vengan por POST o vacíos
     form = SimuladorForm(request.POST or None)
-    resultados = None
-    mensaje = None
+    grafica_html = None
 
     if request.method == 'POST' and form.is_valid():
-        metodo_elegido = form.cleaned_data['metodo']
+        params = {
+            'k1': form.cleaned_data['k1'],
+            'k2': form.cleaned_data['k2'],
+            'Tmax': form.cleaned_data['t_max'],
+            'dt': form.cleaned_data['dt'],
+            'Sw_inj': form.cleaned_data['sw_inj'],
+            'Sw_ini': form.cleaned_data['sw_ini'],
+        }
         
-        if metodo_elegido == 'CN':
-            # Ejecutamos Crank-Nicolson (Punto 1)
-            params = {
-                'k1': form.cleaned_data['k1'],
-                'k2': form.cleaned_data['k2'],
-                'Tmax': form.cleaned_data['t_max'],
-                'dt': form.cleaned_data['dt'],
-                'Sw_inj': form.cleaned_data['sw_inj'],
-                'Sw_ini': form.cleaned_data['sw_ini'],
-            }
-            resultados = ejecutar_simulacion(params)
-        else:
-            mensaje = "El método de Diferencias Finitas está en desarrollo."
+        # Ejecutar modelo matemático
+        res = ejecutar_simulacion(params)
+        
+        # Crear la gráfica con Plotly
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=res['x'], y=res['Sw1'], name='Capa 1 (Sw1)', line=dict(color='#4DD161')))
+        fig.add_trace(go.Scatter(x=res['x'], y=res['Sw2'], name='Capa 2 (Sw2)', line=dict(color='#4DBDD1')))
+        
+        fig.update_layout(
+            title="Saturación de Agua en Medios Porosos",
+            xaxis_title="Distancia [m]",
+            yaxis_title="Saturación (Sw)",
+            template="plotly_white"
+        )
+
+        # Convertir la figura a HTML para insertarla en el template
+        grafica_html = plot(fig, output_type='div', include_plotlyjs=False)
 
     return render(request, 'web/index.html', {
-        'form': form, 
-        'resultados': resultados,
-        'mensaje': mensaje
+        'form': form,
+        'grafica': grafica_html
     })
